f99a7b99f5be33439e40b5f563ebb10e640a2529
Masterain <i@irain.in>
2023-12-16 04:11:18 -0800

    Initial commit

---
+++ AlistClient/AlistClient.py
@@ -0,0 +1,99 @@
+import httpx
+import urllib.parse
+
+
+class AlistClient:
+    def __init__(self, host: str, username: str, password: str):
+        self.host = host
+        self.__username__ = username
+        self.__password__ = password
+        self.client = httpx.Client(base_url=f"https://{self.host}")
+        self.__token__ = self.__get_token__()
+
+    def __get_token__(self):
+        response = self.client.post("/api/auth/login",
+                                    json={"username": self.__username__,
+                                          "password": self.__password__
+                                          })
+        response_json = response.json()
+        if response_json["code"] == 200:
+            return response_json["data"]["token"]
+        else:
+            raise ValueError(f"Failed to get token; {response_json['message']}")
+
+    def stream_upload(self, local_file_path: str, target_dir: str, overwrite: bool = True):
+        file_name = local_file_path.split("/")[-1]
+        if file_name.endswith(".png"):
+            MMIE_type = "image/png"
+        else:
+            MMIE_type = "application/octet-stream"
+        remote_path = f"{target_dir}/{file_name}"
+        if not overwrite:
+            metadata_response = self.fetch_metadata(remote_path)
+            if metadata_response["code"] == 200:
+                file_bytes = open(local_file_path, "rb").read()
+                local_file_size = len(file_bytes)
+                if metadata_response["data"]["size"] == local_file_size:
+                    return {"code": 204, "message": "File already exist, skip as user defined setting."}
+        file_bytes = open(local_file_path, "rb").read()
+        url_encoded_path = urllib.parse.quote(remote_path)
+        header = {
+            "Authorization": self.__token__,
+            "Content-Type": MMIE_type,
+            "Content-Length": str(len(file_bytes)),
+            "File-Path": url_encoded_path,
+            "As-Task": "false"
+        }
+        response = self.client.put("/api/fs/put", headers=header, content=file_bytes, timeout=120)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise ValueError(f"Failed to upload file; {response.content}")
+
+    def list_path(self, path: str, password: str = None):
+        header = {
+            "Authorization": self.__token__,
+        }
+        body = {
+            "path": path,
+        }
+        if password:
+            body["password"] = password
+        res = self.client.post("/api/fs/dirs", headers=header, json=body)
+        return res.json()
+
+    def fetch_metadata(self, path: str, password: str = None):
+        header = {
+            "Authorization": self.__token__,
+        }
+        body = {
+            "path": path,
+        }
+        if password:
+            body["password"] = password
+        res = self.client.post("/api/fs/get", headers=header, json=body)
+        return res.json()
+
+    def delete_file(self, file_name: str | list[str], target_dir: str):
+        header = {
+            "Authorization": self.__token__,
+        }
+        if type(file_name) is str:
+            file_name = [file_name]
+        body = {
+            "name": file_name,
+            "dir": target_dir,
+        }
+        res = self.client.post("/api/fs/remove", headers=header, json=body)
+        return res.json()
+
+    def create_dir(self, target_dir: str):
+        header = {
+            "Authorization": self.__token__,
+            "Content-Type": "application/json"
+        }
+        body = {
+            "path": target_dir,
+        }
+        res = self.client.post("/api/fs/mkdir", headers=header, json=body)
+        return res.json()
---
+++ main.py
@@ -0,0 +1,89 @@
+import os
+import concurrent.futures
+import httpx
+import argparse
+from AlistClient.AlistClient import AlistClient
+from dotenv import load_dotenv
+
+load_dotenv()
+client = AlistClient(os.getenv("ALIST_HOST"), os.getenv("ALIST_USERNAME"), os.getenv("ALIST_PASSWORD"))
+
+
+def get_all_files(directory):
+    file_paths = []
+    for root, dirs, files in os.walk(directory):
+        for f in files:
+            file_paths.append(os.path.join(root, f))
+    return file_paths
+
+
+def upload_file_executor(local_file_path: str, target_path: str, client_instance: AlistClient, overwrite: bool = True):
+    print(f"Starting upload task: {local_file_path}")
+    this_subdir = (local_file_path.replace("Snap.Static-main/", "").
+                   replace("Snap.Static.Zip-main", "").
+                   replace(local_file_path.split("/")[-1], ""))
+    this_subdir = "" if this_subdir == "/" else this_subdir
+    while True:
+        try:
+            res = client_instance.stream_upload(local_file_path, target_path + this_subdir, overwrite)
+            print(f"Upload {local_file_path} result: {res}")
+            break
+        except httpx.ReadTimeout:
+            print(f"Upload {local_file_path} ReadTimeout. Please check this later.")
+
+
+def zip_resource_handler(overwrite: bool = True):
+    result = client.create_dir("/zip")
+    print(f"Create /zip result: {result}")
+
+    list_dir = list(f"Snap.Static.Zip-main/{f}" for f in os.listdir("Snap.Static.Zip-main") if f.endswith(".zip"))
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=os.cpu_count()) as executor:
+        futures = {executor.submit(upload_file_executor, file, "/zip/", client, overwrite) for file in list_dir}
+        done, not_done = concurrent.futures.wait(futures, timeout=120)
+
+
+def raw_resource_handler(overwrite: bool = True):
+    # list all files in folder and subfolder under /Snap.Static-main
+    raw_file = list(file.replace("\\", "/") for file in get_all_files("Snap.Static-main") if file.endswith(".png"))
+    all_remote_sub_dirs = []
+    for file in raw_file:
+        file_name = file.split("/")[-1]
+        subdir = file.replace("Snap.Static-main/", "").replace(file_name, "")
+        file_remote_path = f'/raw/{subdir}'
+        all_remote_sub_dirs.append(file_remote_path)
+    all_remote_sub_dirs = list(set(all_remote_sub_dirs))
+
+    for sub_dir in all_remote_sub_dirs:
+        result = client.list_path(sub_dir)
+        if "not found" in result["message"]:
+            print(f"{sub_dir} not found. Creating...")
+            result = client.create_dir(sub_dir)
+            print(f"Create {sub_dir} result: {result}")
+        else:
+            print(f"{sub_dir} found.")
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=os.cpu_count()) as executor:
+        futures = {executor.submit(upload_file_executor, file, "/raw/", client, overwrite) for file in raw_file}
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Upload resource to Alist")
+    parser.add_argument("--zip", action="store_true", help="Upload zip file to /zip")
+    parser.add_argument("--raw", action="store_true", help="Upload raw file to /raw")
+    parser.add_argument("--overwrite", action="store_true", help="Overwrite existing file")
+    args = parser.parse_args()
+    if args.overwrite:
+        overwrite = True
+    else:
+        overwrite = False
+    if not args.zip and not args.raw:
+        print("Please specify --zip or --raw")
+    if args.zip:
+        zip_resource_handler(overwrite)
+    if args.raw:
+        raw_resource_handler(overwrite)
+
+
+if __name__ == "__main__":
+    main()
